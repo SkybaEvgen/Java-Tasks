ca965661cfe392c178ab1ff6fdba3468511aca4d
Evgen <evgen.skyba@gmail.com>
2020-02-05 14:54:27 +0100

    solution

---
+++ 2019_01_Tasks_JavaRush/src/Read_file_2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Минимальный байт
+*/
+
+public class Read_file_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        Set<Integer> setBites = new TreeSet<>();
+        List<Integer> listTemp = new ArrayList<>();
+
+        while (inputStream.read() > 0) {
+            int data = inputStream.read();
+            listTemp.add(data);
+            setBites.add(data);
+        }
+        inputStream.close();
+
+        System.out.println(setBites.iterator().next());  // выводим первый элемент
+        System.out.println(listTemp);
+    }
+}
