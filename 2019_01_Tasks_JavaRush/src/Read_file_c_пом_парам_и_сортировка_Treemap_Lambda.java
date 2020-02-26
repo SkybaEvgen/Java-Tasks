51eb48ce0e0cca16aa15d56ee18e972e17390f36
Evgen <evgen.skyba@gmail.com>
2020-02-26 15:08:12 +0100

    read file

--- 2019_01_Tasks_JavaRush/src/Read_file_c_пом_парам_и_сортировка_Treemap_Lambda.java
+++ 2019_01_Tasks_JavaRush/src/Read_file_c_пом_парам_и_сортировка_Treemap_Lambda.java
@@ -1,66 +1,56 @@
 /*
-Считаем зарплаты
-
+Самый богатый
 В метод main первым параметром приходит имя файла.
 В этом файле каждая строка имеет следующий вид:
 имя значение
 где [имя] - String, [значение] - double. [имя] и [значение] разделены пробелом.
 
 Для каждого имени посчитать сумму всех его значений.
-Все данные вывести в консоль, предварительно отсортировав в возрастающем порядке по имени.
+Вывести в консоль имена в алфавитном порядке, у которых максимальная сумма.
+Имена разделять пробелом либо выводить с новой строки.
 Закрыть потоки.
 
 Пример входного файла:
-Петров 2
-Сидоров 6
+Петров 0.501
 Иванов 1.35
-Петров 3.1
+Петров 0.85
 
 Пример вывода:
-Иванов 1.35
-Петров 5.1
-Сидоров 6.0
+Петров
 
 
 Требования:
 1. Программа НЕ должна считывать данные с консоли.
 2. Программа должна считывать содержимое файла (используй FileReader).
 3. Поток чтения из файла (FileReader) должен быть закрыт.
-4. Программа должна выводить в консоль каждое имя и сумму всех его значений, все данные должны быть отсортированы в возрастающем порядке по имени.
+4. Программа должна выводить в консоль имена, у которых максимальная сумма.
 */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
-public class Read_file_c_помощью_параметра_и_сортировка_Treemap {
+public class Read_file_c_пом_парам_и_сортировка_Treemap_Lambda {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-        Map<String, Double> map = new TreeMap<>();
+        Map<String, Double> map = new HashMap<>();
+        List<String> listName = new ArrayList<>();
 
         while (reader.ready()) {
             String s = reader.readLine();
             String[] words = s.split("\\s");
-            if (map.containsKey(words[0])) {
-                double d = map.get(words[0]);
-                map.put(words[0], d + Double.parseDouble(words[1]));
-            } else map.put(words[0], Double.parseDouble(words[1]));
-
-            /*
-            second way (вместо if else)
             map.merge(words[0], Double.valueOf(words[1]), Double::sum);
-             */
         }
 
+        map.forEach((s, d) -> {
+            if (d.equals(Collections.max(map.values()))) {
+                System.out.println(s);
+            }
+        });
 
-
-
-        for (Map.Entry entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
 
         reader.close();
+
     }
 }
